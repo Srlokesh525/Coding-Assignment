07ae14c7b3e1ee6f40f4fe2a67f0095c669880d7
Srlokesh525 <130358809+Srlokesh525@users.noreply.github.com>
2023-10-14 16:30:36 +0530

    Add files via upload

---
+++ Coding Assignment/Java script.js
@@ -0,0 +1,29 @@
+Javascript 
+1.function reverseWords(sentence) {
+  // Split the sentence into words
+  const words = sentence.split(" ");
+  
+  // Reverse each word
+  const reversedWords = words.map(word => {
+    return word.split("").reverse().join("");
+  });
+
+  // Join the reversed words back into a sentence
+  const reversedSentence = reversedWords.join(" ");
+
+  return reversedSentence;
+}
+
+// Example usage:
+const inputSentence = "Hello World";
+const reversedSentence = reverseWords(inputSentence);
+console.log(reversedSentence); // Outputs "olleH dlroW"
+
+2.const originalArray = [5, 2, 9, 1, 5, 6];
+
+// Sort the array in descending order
+originalArray.sort(function(a, b) {
+  return b - a;
+});
+
+console.log(originalArray); // Outputs: [9, 6, 5, 5, 2, 1]
---
+++ Coding Assignment/Java.java
@@ -0,0 +1,106 @@
+Java 
+1.import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create an array with the values (1, 2, 3, 4, 5, 6, 7)
+        Integer[] numbers = {1, 2, 3, 4, 5, 6, 7};
+
+        // Convert the array to a list for shuffling
+        List<Integer> numberList = new ArrayList<>(Arrays.asList(numbers));
+
+        // Shuffle the list
+        Collections.shuffle(numberList);
+
+        // Print the shuffled list
+        System.out.println("Shuffled Array: " + numberList);
+    }
+}
+
+
+2.import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Roman Number: ");
+        String romanNumeral = scanner.nextLine();
+
+        int result = romanToInteger(romanNumeral);
+        System.out.println("Integer value: " + result);
+    }
+
+    public static int romanToInteger(String s) {
+        // Create a mapping of Roman numerals to their integer values
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currValue = romanMap.get(s.charAt(i));
+
+            if (currValue < prevValue) {
+                result -= currValue;
+            } else {
+                result += currValue;
+            }
+
+            prevValue = currValue;
+        }
+
+        return result;
+    }
+}
+
+3.import java.util.Scanner;
+
+public class PangramChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+
+        boolean isPangram = isPangram(sentence);
+        if (isPangram) {
+            System.out.println("The input is a pangram.");
+        } else {
+            System.out.println("The input is not a pangram.");
+        }
+    }
+
+    public static boolean isPangram(String sentence) {
+        // Create an array to track the presence of each letter (A-Z)
+        boolean[] alphabetPresent = new boolean[26];
+
+        // Convert the sentence to lowercase for case-insensitive comparison
+        sentence = sentence.toLowerCase();
+
+        // Iterate through the sentence and mark each alphabet letter as present
+        for (char c : sentence.toCharArray()) {
+            if ('a' <= c && c <= 'z') {
+                alphabetPresent[c - 'a'] = true;
+            }
+        }
+
+        // Check if all alphabet letters are present
+        for (boolean letterPresent : alphabetPresent) {
+            if (!letterPresent) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
